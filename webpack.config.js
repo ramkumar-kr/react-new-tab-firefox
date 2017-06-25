c6debd5c9ad6a0f778198f1b81fe978d2338454c
Ramkumar <ramkumar.kr94@gmail.com>
2017-06-25 10:21:51 +0530

    - Update styles to that the page looks less cluttered - Update webpack to 3.0 - reduces artifact size from ~65kB to ~45kB

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: __dirname + '/app/index.html',
     filename: __dirname + '/dist/index.html',
-    inject: 'body'
+    inject: 'body',
+    minify: {
+        collapseWhitespace: true,
+        removeComments: true
+    }
 });
+var UglifyJSPluginConfig = new UglifyJSPlugin({
+    compress: {
+        warnings: false,
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+    }
+})
 
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var CopyWebpackPluginConfig = new CopyWebpackPlugin([
@@ -28,5 +42,5 @@ module.exports = {
        {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
     ]
   },
-  plugins: [HtmlWebpackPluginConfig, CopyWebpackPluginConfig]
+  plugins: [HtmlWebpackPluginConfig, CopyWebpackPluginConfig, UglifyJSPluginConfig]
 }
