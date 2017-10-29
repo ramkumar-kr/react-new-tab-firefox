729d8a46c3db67fc464cdadb26f2247233e99b17
Ramkumar <ramkumar.kr94@gmail.com>
2017-10-29 10:12:08 +0530

    Fix hound issues

--- app/options.js
+++ app/options.js
@@ -1,12 +1,12 @@
 var updateStyle = function () {
   var css = document.getElementById("reactCss").value;
   browser.storage.local.set({ "style": css });
-}
+};
 
 browser.storage.local.get("style").then((css) => {
-  if (css.style != undefined) {
+  if (css.style !== undefined) {
     document.getElementById("reactCss").value = css.style; 
   }
-})
+});
 
-document.getElementById("styleForm").addEventListener("submit", updateStyle)
+document.getElementById("styleForm").addEventListener("submit", updateStyle);
