fecb62d2ae34670093f8dbead22270a5b824acc8
Ram Kumar K R <4789010+ramkumar-kr@users.noreply.github.com>
2020-10-11 13:50:28 -0700

    Update packages and dependencies (#11)

--- app/options.js
+++ app/options.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import BookmarkFolder from "./components/bookmarkFolder";
+
+class BookmarkFolder extends Component {
+    
+  async setBookmark() {
+      const selectedOption = document.getElementById("newBookmark").value;
+      browser.storage.local.set({"bookmarkId": selectedOption});
+      window.location.reload();
+  }
+  render() {
+      var options = [];
+      var options = this.props.folders.map((e) => {
+          return (
+              <option value={e.id}>{e.title}</option>
+          );
+      });
+      return(
+          <div>
+              <h3>Selected Bookmark Folder </h3> <em>{this.props.bookmark.title}</em>
+              <h3> Select a new Bookmark Folder </h3>
+              <select id="newBookmark" >
+                  {options}
+              </select>
+
+              <button onClick={this.setBookmark}>Update</button>
+          </div>
+      );
+  }
+}
+
 
 
 var updateStyle = function () {
